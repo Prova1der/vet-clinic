5ac65f9a03190545a07265e927f920104337e39b
VD <101dashutin@gmail.com>
2024-02-19 16:50:39 +0200

    REQ-10: Add a few clients and print them (grouped by cities)

--- src/main/java/magicvet/component/ApplicationRunner.java
+++ src/main/java/magicvet/component/ApplicationRunner.java
@@ -1,51 +1,12 @@
 package main.java.magicvet.component;
 
-import main.java.magicvet.Main;
-import main.java.magicvet.model.Client;
-import main.java.magicvet.model.Pet;
-import main.java.magicvet.service.ClientService;
-import main.java.magicvet.service.PetService;
-
 public class ApplicationRunner {
 
-    private final ClientService clientService = new ClientService();
-    private final PetService petService = new PetService();
+    private final EntityRegister register = new EntityRegister();
 
     public void run() {
         if (Authenticator.auth()) {
-            Client client = clientService.resiterNewClient();
-
-            if (client != null) {
-                registerPets(client);
-                System.out.println(client);
-            }
+            register.registerClients();
         }
     }
-
-    private void registerPets(Client client){
-        boolean continueAddPets = true;
-
-        while (continueAddPets) {
-            addPet(client);
-
-            System.out.print("Do you want add more pets for the current client? (y/n): ");
-            String answer = Main.SCANNER.nextLine();
-
-            if("n".equals(answer)) {
-                continueAddPets = false;
-            }
-        }
-    }
-
-    private void addPet(Client client) {
-        System.out.println("Adding a new pet.");
-
-        Pet pet = petService.registerNewPet();
-        if (pet != null) {
-            client.addPet(pet);
-            pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
-            System.out.println("Pet has been added.");
-        }
-    }
-
 }
